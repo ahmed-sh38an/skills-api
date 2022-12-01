47964b847526ff8cebe2b13b98f293c858751f41
Ahmed-Sh38an <ahmed.sh38an@gmail.com>
2022-12-02 00:36:15 +0200

    first commit

---
+++ vue-rest-api/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main
+    class="bg-white rounded-xl font-bold font-serif text-center min-h-fit py-10"
+  >
+    <h1 class="text-7xl">Skills App</h1>
+    <h2 class="text-5xl">Built with Vue.JS Frontend</h2>
+    <h2 class="text-5xl">and Laravel Backend as API</h2>
+  </main>
+</template>
